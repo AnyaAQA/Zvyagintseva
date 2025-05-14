e1552bbc93f49ea2bc720b6a3f17a7a00fd2e1e9
Аня <anazvyagintzeva@yandex.ru>
2025-05-14 20:46:45 +0300

    Задача на тему "Переменные.Операция срвнения"

---
+++ Homework3.py
@@ -0,0 +1,13 @@
+
+#Задача 4 (Операции сравнения — простая версия)
+#Напиши код, который:
+#Объявляет две переменные:
+#temperature = 28
+#threshold = 30
+#Проверяет, что temperature строго меньше threshold, и выводит результат (True или False).
+#Дополнительно: выведи тип этого результата с помощью type().
+
+temperature = 28
+threshold = 30
+print (temperature < threshold)
+print(type(temperature < threshold))
